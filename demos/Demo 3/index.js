8db957016fc75c270cde5d94a585a5690febdf04
Kevin Heritage <kevin.heritage@entelect.co.za>
2020-09-06 20:20:29 +0200

    Create demo for collection functions

--- demos/Demo 3/index.js
+++ demos/Demo 3/index.js
@@ -1,1 +1,63 @@
-console.log('Create this demo right here');
+const people = [
+  {
+    name: "Kevin",
+    age: 55,
+  },
+  {
+    name: "Luke",
+    age: 45,
+  },
+  {
+    name: "Tim",
+    age: 21,
+  },
+];
+
+const theForeach = () => {
+  console.log("Foreach");
+  people.forEach((person) => console.log("Hello ", person.name));
+};
+
+const theFind = () => {
+  console.log("Find");
+  const kevin = people.find((person) => person.name === "Kevin");
+  console.log("Kevin: ", kevin);
+};
+
+const theMap = () => {
+  console.log("Map");
+  const justNames = people.map((person) => ({
+    name: person.name,
+    nameWithAge: [person.name, person.age].join(" "),
+  }));
+  console.log("Just Names: ", justNames);
+};
+
+const theFilter = () => {
+  console.log("Filter");
+  const peopleWithK = people.filter((person) =>
+    person.name.toUpperCase().includes("K")
+  );
+  console.log("People With K: ", peopleWithK);
+};
+
+const theSome = () => {
+  console.log("Some");
+  const doesContainSomeoneWithK = people.some((person) =>
+    person.name.toUpperCase().includes("K")
+  );
+  console.log("Does contain someone with K: ", doesContainSomeoneWithK);
+};
+
+const theReduce = () => {
+  console.log("Reduce");
+  const totalAge = people.reduce((total, person) => total + person.age, 0);
+  console.log("Total age: ", totalAge);
+};
+
+const bananaSplit = () => {
+  console.log("Split");
+  const bananas = "Kevin)Luke)Tim";
+  const arrayOfPeople = bananas.split(")");
+  console.log("Array of people: ", arrayOfPeople);
+};
