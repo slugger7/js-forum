f7d0ccec3dbac83cc946de6dfe9482c1aa42417b
Kevin Heritage <kheritage222gmail.com>
2020-09-20 20:12:49 +0200

    CReate callback demo

---
+++ Basics Part 2/Demos/Demo 2 - Async/error-first-callbacks/index.js
@@ -0,0 +1,32 @@
+const fetchPeople = (callback) => {
+    const people = ["Kevin", "Luke", "Tim"];
+    setTimeout(callback(null, people), 3000);
+};
+
+const ourCallback = (err, people) => {
+    if (err) {
+        console.error("We had some issues fetching our people");
+        return; // handle the error dont swallow it
+    }
+
+    console.log("We Have the people", { people });
+}
+
+console.log("Before we call our async function");
+
+fetchPeople(ourCallback);
+
+console.log("We have called our async function");
+
+/*
+    change to curried version
+
+    const ourCallback = (index) => (err, people) => {
+        if (err) {
+            console.error("We had some issues fetching our people");
+            return;
+        }
+
+        console.log(`We have the person at ${index}`, { person: people[index]});
+    }
+*/
